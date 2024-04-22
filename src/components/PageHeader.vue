d7e6f9b9366ef4012758ebdd89724d46323f8669
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-22 15:59:20 +0200

    feat: add dinamic and responsive header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,18 +1,29 @@
 <script>
 export default {
-    name: "PageHeader"
+    name: "PageHeader",
+    props: ["info", "images"],
+    methods: {
+        //Funzione per stampare l'immagine dinamicamente
+        getImgByUrl: function(relative) {
+            return new URL(relative, import.meta.url).href;
+        }
+    }
 }
+  
+
 </script>
 
 <template>
 <div class="ms-header d-flex px-3 justify-content-between align-items-center">
-    <div class="logo">ciao</div>
+    <div class="logo">
+        <img :src=getImgByUrl(images.logoUrl) alt="logo">
+    </div>
     <nav>
-        <ul class="d-flex gap-1 mb-0">
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
+        <ul class="d-flex gap-4 mb-0">
+            <li v-for="section in info" class="sections">{{ section.name }}</li>
+            <li>
+                <img :src="getImgByUrl(images.imageUrl)" alt="logo">
+            </li>
         </ul>
     </nav>
 </div>
@@ -20,7 +31,20 @@ export default {
 
 <style scoped lang="scss">
 .ms-header {
-    border: 2px solid black;
     height: 70px;
+}
+
+.logo {
+    width: 80px;
+    height: 30px;
+    img {
+        width: 100%;
+        height: 100%;
+    }
+}
+@media screen and (max-width: 768px) {
+    .sections{
+        display: none;
+    }
 }
 </style>
