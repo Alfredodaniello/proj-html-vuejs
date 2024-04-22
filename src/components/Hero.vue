e38d628b9947ac9a159258bf06280d2da9ea1ec5
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-22 19:49:22 +0200

    feat: add img with position absolute and scaffolding completed (need style)

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,24 +5,50 @@ export default {
 </script>
 
 <template>
-<div class="container-fluid">
+<div class="container-fluid position-relative">
     <div class="row justify-content-center">
-        <div class="col-4">
+        <div class="col-4 px-5">
             <h1>Devotion that Never ends</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolorem</p>
             <button>Read More</button>
         </div>
         <div class="col-4">
-            <div class="img-container d-flex">
-                <div class="woman">
-                    <img src="../assets/img/h-2-slider-img-15.png" alt="">
+            <div class="img-container position-relative">
+                <div class="d-flex px-4">
+                    <div class="woman">
+                        <img src="../assets/img/h-2-slider-img-15.png" alt="">
+                    </div>
+                    <div class="man">
+                        <img src="../assets/img/h-2-slider-img-16.png" alt="">
+                    </div>
                 </div>
-                <div class="man">
-                    <img src="../assets/img/h-2-slider-img-16.png" alt="">
+                    <div class="icons icon-bottom position-absolute">
+                    <img src="../assets/img/h-2-slider-img-13.png" alt="">
                 </div>
-                
-                
+                <div class="icons icon-center position-absolute">
+                    <img src="../assets/img/h-2-slider-img-12.png" alt="">
+                </div>
+                <div class="icons icon-top position-absolute">
+                    <img src="../assets/img/h-2-slider-img-12.png" alt="">
+                </div>
+                <div class="icons top-right position-absolute">
+                    <img src="../assets/img/h-2-slider-img-14.png" alt="">
+                </div>
+                <div class="icons center-right position-absolute">
+                    <img src="../assets/img/short-slider-rev-1-img-6.png" alt="">
+                </div>
+                <div class="icons position-absolute bottom-right">
+                    <img src="../assets/img/h-2-slider-img-17.png" alt="">
+                </div>
+
             </div>
+        </div>
+            <div class="circle circle-right  d-flex justify-content-center align-items-center position-absolute"><i class="fa-solid fa-chevron-right"></i></div>
+            <div class="circle circle-left d-flex justify-content-center align-items-center position-absolute"><i class="fa-solid fa-chevron-left"></i></div>
+        <div class="col-12 d-flex justify-content-center mt-4 gap-4">
+            <div><i class="fa-solid fa-circle"></i></div>
+            <div><i class="fa-solid fa-circle"></i></div>
+            <div><i class="fa-solid fa-circle"></i></div>
         </div>
     </div>
 </div>
@@ -36,4 +62,57 @@ img {
     width: 100%;
     height: 100%;
 }
+.icons {
+    width: 50px;
+    height: 50px;
+}
+.icon-bottom {
+    bottom: 0%;
+    left: -0%;
+}
+.icon-center {
+    top: 40%;
+    left: -10%;
+}
+.icon-top{
+    top: -15%;
+    left:0%;
+}
+
+.top-right{
+    right: 14%;
+    top: -20%;
+}
+
+.center-right{
+    right: 7%;
+    top: 18%;
+}
+
+.bottom-right{
+    right: -2%;
+    bottom: 0;
+}
+
+i {
+    font-size: 8px;
+}
+
+.circle {
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    background-color: aqua;
+}
+
+.circle-right {
+    top: 35%;
+    right: 1%;
+}
+
+.circle-left{
+    top: 35%;
+    left: 1%;
+}
+
 </style>
