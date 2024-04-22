21131682464afa1bc69151666d993b25b66d2ccc
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-22 12:33:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+import PageHeader from "./components/PageHeader.vue";
+export default {
+  name: "App",
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<template>
+<div>
+  <header>
+    <PageHeader></PageHeader>
+  </header>
+  
+</div>
+</template>
+
+<style lang="scss">
+@use "./style/partials/generic" as *;
+@use "./style/partials/variables" as *;
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "PageHeader"
+}
+</script>
+
+<template>
+<div class="ms-header d-flex px-3 justify-content-between align-items-center">
+    <div class="logo">ciao</div>
+    <nav>
+        <ul class="d-flex gap-1 mb-0">
+            <li>ciao</li>
+            <li>ciao</li>
+            <li>ciao</li>
+            <li>ciao</li>
+        </ul>
+    </nav>
+</div>
+</template>
+
+<style scoped lang="scss">
+.ms-header {
+    border: 2px solid black;
+    height: 70px;
+}
+</style>
