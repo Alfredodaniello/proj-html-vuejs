6852c6d75d54fc88cae0db342732fab4ce181fd5
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-23 11:25:51 +0200

    feat: add Calendar component (need fix style)

---
+++ src/components/Contact.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "Contact",
+}
+</script>
+
+<template>
+<div class="container-fluid ms-container">
+    <div class="row">
+        <div class="col-10 m-auto text-center mt-5">
+            <h1>Want to work with us? Send us a message!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias veritatis non facere quia accusantium, tenetur eligendi sequi perspiciatis repudiandae fugiat laborum! Vitae rerum hic ipsa suscipit, aut atque eveniet.</p>
+            <button>Contact</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+.ms-container {
+    height:300px;
+    background-image: url("../assets/img/h-2-background-1.jpg");
+    object-fit: auto;
+}
+</style>
--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -2,12 +2,16 @@
 import Hero from "./Hero.vue";
 import Banner from "./Banner.vue";
 import WorkingProcess from "./WorkingProcess.vue";
+import Contact from "./Contact.vue";
+import Calendar from "./Calendar.vue";
 export default {
     name: "PageMain",
     components: {
         Hero,
         Banner,
-        WorkingProcess
+        WorkingProcess,
+        Contact,
+        Calendar
     }
 }
 </script>
@@ -16,6 +20,8 @@ export default {
 <Hero></Hero>
 <Banner></Banner>
 <WorkingProcess></WorkingProcess>
+<Contact></Contact>
+<Calendar></Calendar>
 </template>
 
 <style scoped lang="scss">
